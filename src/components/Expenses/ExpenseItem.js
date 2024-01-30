5887f9ba775ae86814ec6823e66da48824fafe32
tonissonm <matistonisson@gmail.com>
2024-01-30 13:47:14 +0200

    Ülesanne 10

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
-function ExpenseItem(props){
+import Card from '../UI/Card'
+import { useState } from 'react';
+const ExpenseItem =(props) =>{
+    const [title,setTitle] = useState(props.expenseData.title);
+    const clickHandler =()=>{
+        console.log("Clicked!");
+        setTitle(`Updated by click ${title}`);
+        console.log(title);
+    }
     return(
-        <div className="expense-item">
-            <ExpenseDate date ={props.expensedata.date}/>
+        <Card className="expense-item">
+            <ExpenseDate date ={props.expenseData.date}/>
             <div className ="expense-item__description">
-                <h2>{props.expensedata.title}</h2>
-                <div className="expense-item__price">{props.expensedata.price}</div>
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}</div>
             </div>
-        </div>
+            <button onClick={clickHandler}>Click me!</button>
+        </Card>
 );
 }
 export default ExpenseItem;
