00b8c56858eeb7aa1b22a61f8038b6fc7c84b80c
tonissonm <matistonisson@gmail.com>
2024-01-30 12:55:05 +0200

    ÜL 7-8 lehed

---
+++ src/Expenses.js
@@ -0,0 +1,12 @@
+import './Expenses.css'
+import Card from './Card'
+import ExpenseItem from "./ExpenseItem";
+function Expenses(props){
+    return(
+        <Card className="expenses">
+            <ExpenseItem expenseData={props.expenses[0]}/>
+            <ExpenseItem expenseData={props.expenses[1]}/>
+        </Card>
+    )
+}
+export default Expenses
